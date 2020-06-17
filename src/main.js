4f933dc259ac8052deaae5c268b8bf2e4cc52fbc
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-06-17 20:06:10 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-fetch('https://ryoga-exe.github.io/canvas-assist-js/src/js/canvas-assist.js').then(r=>r.text()).then(t=>eval(t)).then(()=>{
+fetch('https://github.com/Ryoga-exe/canvas-assist-js/blob/master/src/js/canvas-assist.js').then(r=>r.text()).then(t=>eval(t)).then(()=>{
     createCanvas('canvas');
 });
